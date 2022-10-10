a629a07e6edda9a57de07dea63d3ae7f5e9a239e
Matt Myers <myersmatt1987@gmail.com>
2022-10-10 10:18:56 -0700

    Page now displays a list of users

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../interfaces/hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +8,15 @@ import {Hero} from "../interfaces/hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes: Hero[] | undefined;
+  // hero : Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
   constructor() { }
 
   ngOnInit(): void {
+    this.heroes = HEROES;
   }
 
 }

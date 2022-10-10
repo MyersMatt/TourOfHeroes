e12761d946e45114accba1b11ae8a8a26131a427
Matt Myers <myersmatt1987@gmail.com>
2022-10-10 13:56:26 -0700

    Added a back button after navigating to hero details

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../interfaces/hero";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {HeroService} from "../services/hero.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,21 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero?:Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private  route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
 }

004ef2909bb3dc7ada9d08be00c997551c6381af
Matt Myers <myersmatt1987@gmail.com>
2022-10-11 14:21:18 -0700

    Users now have the ability to update and add new heroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -12,7 +12,7 @@ import {catchError, map, tap} from "rxjs";
 export class HeroService {
 
   private heroesUrl = 'api/heroes';
-
+  httpOptions = { headers: new HttpHeaders({'Content-Type':'application/json'})}
   constructor(private http: HttpClient,private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
@@ -24,9 +24,12 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h=> h.id === id)!;
-    this.log(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    this.log(`HeroService: attempting to fetched hero with id=${id}`);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string): void{
@@ -39,5 +42,19 @@ export class HeroService {
       this.log(`${operation} failed: ${error.message}`);
       return of(result as T);
     }
+  }
+
+  updateHero(hero: Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_=> this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>(`updateHero`))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 }
